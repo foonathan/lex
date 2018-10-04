ac5170c6cc6ac5eb56c4f1e234ebec3bbebfb300
Jonathan Müller <git@foonathan.net>
2018-10-04 11:44:08 +0200

    Add string macro implementation using GCC literal extension
    My IDE runs out of memory trying to process the macro variant.

--- include/foonathan/lex/detail/string.hpp
+++ include/foonathan/lex/detail/string.hpp
@@ -7,6 +7,42 @@
 
 #include <type_traits>
 
+#if defined(__GNUC__)
+namespace foonathan
+{
+namespace lex
+{
+    namespace detail
+    {
+        template <char... Chars>
+        struct string_literal
+        {
+            template <template <char...> class String>
+            using convert_to = String<Chars...>;
+        };
+
+        template <template <char...> class String, class Literal>
+        using convert_literal_to = typename Literal::template convert_to<String>;
+    } // namespace detail
+} // namespace lex
+} // namespace foonathan
+
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wgnu-string-literal-operator-template"
+
+template <typename Char, Char... Chars>
+constexpr foonathan::lex::detail::string_literal<Chars...> operator""_foonathan_lex_string_udl()
+{
+    return {};
+}
+
+#    define FOONATHAN_LEX_DETAIL_STRING(String, Chars)                                             \
+        foonathan::lex::detail::convert_literal_to<String,                                         \
+                                                   decltype(Chars##_foonathan_lex_string_udl)>
+
+#    pragma GCC diagnostic pop
+
+#else
 namespace foonathan
 {
 namespace lex
@@ -42,40 +78,41 @@ namespace lex
 //
 // the range check inside String<Chars[I]> prevents reading out-of-bound string literal if it is
 // instantiated
-#define FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I)                                         \
-    std::conditional_t<(I < sizeof(Chars) && Chars[I] != 0),                                       \
-                       String<(I >= sizeof(Chars)) ? 0 : Chars[I]>, String<>>
+#    define FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I)                                     \
+        std::conditional_t<(I < sizeof(Chars) && Chars[I] != 0),                                   \
+                           String<(I >= sizeof(Chars)) ? 0 : Chars[I]>, String<>>
 
 // recursively split the string in two
-#define FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I)                                             \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I),         \
-                                       FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I + 1)>
-#define FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I)                                             \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I),             \
-                                       FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I + 2)>
-#define FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I)                                             \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I),             \
-                                       FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I + 4)>
-#define FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I)                                            \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I),             \
-                                       FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I + 8)>
-#define FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I)                                            \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I),            \
-                                       FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I + 16)>
-#define FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I)                                            \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I),            \
-                                       FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I + 32)>
-#define FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I)                                           \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I),            \
-                                       FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I + 64)>
-#define FOONATHAN_LEX_DETAIL_STRING256(String, Chars, I)                                           \
-    foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I),           \
-                                       FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I + 128)>
+#    define FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I)                                         \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I),     \
+                                           FOONATHAN_LEX_DETAIL_STRING_TAKE(String, Chars, I + 1)>
+#    define FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I)                                         \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I),         \
+                                           FOONATHAN_LEX_DETAIL_STRING2(String, Chars, I + 2)>
+#    define FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I)                                         \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I),         \
+                                           FOONATHAN_LEX_DETAIL_STRING4(String, Chars, I + 4)>
+#    define FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I)                                        \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I),         \
+                                           FOONATHAN_LEX_DETAIL_STRING8(String, Chars, I + 8)>
+#    define FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I)                                        \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I),        \
+                                           FOONATHAN_LEX_DETAIL_STRING16(String, Chars, I + 16)>
+#    define FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I)                                        \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I),        \
+                                           FOONATHAN_LEX_DETAIL_STRING32(String, Chars, I + 32)>
+#    define FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I)                                       \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I),        \
+                                           FOONATHAN_LEX_DETAIL_STRING64(String, Chars, I + 64)>
+#    define FOONATHAN_LEX_DETAIL_STRING256(String, Chars, I)                                       \
+        foonathan::lex::detail::string_cat<FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I),       \
+                                           FOONATHAN_LEX_DETAIL_STRING128(String, Chars, I + 128)>
 
 // expands to String<Chars[0], Chars[1], ...>
 // does not include embedded null characters
-#define FOONATHAN_LEX_DETAIL_STRING(String, Chars)                                                 \
-    foonathan::lex::detail::check_string_size<FOONATHAN_LEX_DETAIL_STRING256(String, Chars, 0),    \
-                                              sizeof(Chars) - 1, 256>::type
+#    define FOONATHAN_LEX_DETAIL_STRING(String, Chars)                                             \
+        foonathan::lex::detail::check_string_size<                                                 \
+            FOONATHAN_LEX_DETAIL_STRING256(String, Chars, 0), sizeof(Chars) - 1, 256>::type
+#endif
 
 #endif // FOONATHAN_LEX_DETAIL_STRING_HPP_INCLUDED
