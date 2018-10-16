aad4f881b701bb305294649eb90bc43b8db01351
Jonathan Müller <git@foonathan.net>
2018-10-16 14:11:50 +0200

    Improve CMake

--- external/external.cmake
+++ external/external.cmake
@@ -2,17 +2,22 @@
 # This file is subject to the license terms in the LICENSE file
 # found in the top-level directory of this distribution.
 
+option(FOONATHAN_LEX_FORCE_FIND_PACKAGE "force find_package() instead of using git submodule" OFF)
 set(dependency_via_submodule OFF)
 
-find_package(debug_assert QUIET)
-if(NOT debug_assert_FOUND)
-    set(dependency_via_submodule ON)
-    if(TARGET debug_assert)
-        message(STATUS "Using inherited debug_assert target")
-    else()
-        message(STATUS "Installing debug_assert via submodule")
-        execute_process(COMMAND git submodule update --init -- external/debug_assert
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-        add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/debug_assert EXCLUDE_FROM_ALL)
+if(FOONATHAN_LEX_FORCE_FIND_PACKAGE)
+    find_package(debug_assert REQUIRED)
+else()
+    find_package(debug_assert QUIET)
+    if(NOT debug_assert_FOUND)
+        set(dependency_via_submodule ON)
+        if(TARGET debug_assert)
+            message(STATUS "Using inherited debug_assert target")
+        else()
+            message(STATUS "Installing debug_assert via submodule")
+            execute_process(COMMAND git submodule update --init -- external/debug_assert
+                            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+            add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/debug_assert EXCLUDE_FROM_ALL)
+        endif()
     endif()
 endif()
