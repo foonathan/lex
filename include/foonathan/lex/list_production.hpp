a0ac54172e329993b09157b7537c99dc42776e94
Jonathan Müller <git@foonathan.net>
2019-04-07 15:22:57 +0200

    Add support for empty and trailing lists

--- include/foonathan/lex/list_production.hpp
+++ include/foonathan/lex/list_production.hpp
@@ -15,13 +15,50 @@ namespace foonathan
 namespace lex
 {
     template <class Derived, class Grammar, class Production, class Separator>
-    struct list_production : detail::base_production
+    class list_production : public detail::base_production
     {
         static_assert(is_production<Production>::value, "list element must be a production");
         static_assert(is_token<Separator>::value, "list separator must be a token");
 
-        template <class Func>
-        static constexpr auto parse(tokenizer<typename Grammar::token_spec>& tokenizer, Func&& f)
+        template <class End, class Func>
+        static constexpr auto parse_impl(std::true_type /* allow_empty */,
+                                         tokenizer<typename Grammar::token_spec>& tokenizer,
+                                         Func&&                                   f)
+        {
+            auto result = lex::detail::apply_parse_result(f, Derived{});
+            if (tokenizer.peek().is(End{}))
+                return result;
+
+            auto elem = Production::parse(tokenizer, f);
+            if (elem.is_unmatched())
+                return decltype(result)::unmatched();
+
+            result = lex::detail::apply_parse_result(f, Derived{},
+                                                     result.template value_or_tag<Derived>(),
+                                                     elem.template value_or_tag<Production>());
+
+            while (tokenizer.peek().is(Separator{}))
+            {
+                tokenizer.bump();
+                if (Derived::allow_trailing::value && tokenizer.peek().is(End{}))
+                    break;
+
+                elem = Production::parse(tokenizer, f);
+                if (elem.is_unmatched())
+                    return decltype(result)::unmatched();
+
+                result = lex::detail::apply_parse_result(f, Derived{},
+                                                         result.template value_or_tag<Derived>(),
+                                                         elem.template value_or_tag<Production>());
+            }
+
+            return result;
+        }
+
+        template <class End, class Func>
+        static constexpr auto parse_impl(std::false_type /* allow_empty */,
+                                         tokenizer<typename Grammar::token_spec>& tokenizer,
+                                         Func&&                                   f)
             -> decltype(lex::detail::apply_parse_result(
                 f, std::declval<Derived>(),
                 Production::parse(tokenizer, f).template value_or_tag<Production>()))
@@ -36,6 +73,8 @@ namespace lex
             while (tokenizer.peek().is(Separator{}))
             {
                 tokenizer.bump();
+                if (Derived::allow_trailing::value && tokenizer.peek().is(End{}))
+                    break;
 
                 elem = Production::parse(tokenizer, f);
                 if (elem.is_unmatched())
@@ -47,6 +86,24 @@ namespace lex
             }
 
             return result;
+        }
+
+    public:
+        using end_token      = void;
+        using allow_empty    = std::false_type;
+        using allow_trailing = std::false_type;
+
+        template <class Func>
+        static constexpr auto parse(tokenizer<typename Grammar::token_spec>& tokenizer, Func&& f)
+        {
+            constexpr bool has_end = !std::is_same<typename Derived::end_token, void>::value;
+            static_assert(!Derived::allow_empty::value || has_end,
+                          "an empty list requires an end token");
+            static_assert(!Derived::allow_trailing::value || has_end,
+                          "a list with trailing seperator requires an end token");
+
+            using end = std::conditional_t<has_end, typename Derived::end_token, lex::eof_token>;
+            return parse_impl<end>(typename Derived::allow_empty{}, tokenizer, f);
         }
     };
 } // namespace lex
--- test/list_production.cpp
+++ test/list_production.cpp
@@ -42,7 +42,7 @@ void verify(lex::parse_result<int> result, int expected)
 }
 } // namespace
 
-TEST_CASE("list_production")
+TEST_CASE("list_production: non-empty, non-trailing")
 {
     using grammar = lex::grammar<test_spec, struct A, struct P>;
     struct A : lex::token_production<A, grammar, a>
@@ -88,4 +88,164 @@ TEST_CASE("list_production")
 
     constexpr auto r5 = parse<P>(visitor{}, ",a");
     verify(r5, unmatched);
+}
+
+TEST_CASE("list_production: non-empty, trailing")
+{
+    using grammar = lex::grammar<test_spec, struct A, struct P>;
+    struct A : lex::token_production<A, grammar, a>
+    {};
+    struct P : lex::list_production<P, grammar, A, comma>
+    {
+        using end_token      = lex::eof_token;
+        using allow_trailing = std::true_type;
+    };
+
+    struct visitor
+    {
+        constexpr A operator()(A, a)
+        {
+            return {};
+        }
+
+        constexpr int operator()(P, A)
+        {
+            return 1;
+        }
+        constexpr int operator()(P, int list, A)
+        {
+            return list + 1;
+        }
+
+        constexpr void operator()(lex::unexpected_token<grammar, A, a>,
+                                  const lex::tokenizer<test_spec>&)
+        {}
+    };
+
+    constexpr auto r0 = parse<P>(visitor{}, "");
+    verify(r0, unmatched);
+
+    constexpr auto r1 = parse<P>(visitor{}, "a");
+    verify(r1, 1);
+
+    constexpr auto r2 = parse<P>(visitor{}, "a,a");
+    verify(r2, 2);
+
+    constexpr auto r3 = parse<P>(visitor{}, "a,a,a");
+    verify(r3, 3);
+
+    constexpr auto r4 = parse<P>(visitor{}, "a,");
+    verify(r4, 1);
+
+    constexpr auto r5 = parse<P>(visitor{}, ",a");
+    verify(r5, unmatched);
+}
+
+TEST_CASE("list_production: empty, non-trailing")
+{
+    using grammar = lex::grammar<test_spec, struct A, struct P>;
+    struct A : lex::token_production<A, grammar, a>
+    {};
+    struct P : lex::list_production<P, grammar, A, comma>
+    {
+        using end_token   = lex::eof_token;
+        using allow_empty = std::true_type;
+    };
+
+    struct visitor
+    {
+        constexpr A operator()(A, a)
+        {
+            return {};
+        }
+
+        constexpr int operator()(P)
+        {
+            return 0;
+        }
+        constexpr int operator()(P, int list, A)
+        {
+            return list + 1;
+        }
+
+        constexpr void operator()(lex::unexpected_token<grammar, A, a>,
+                                  const lex::tokenizer<test_spec>&)
+        {}
+    };
+
+    constexpr auto r0 = parse<P>(visitor{}, "");
+    verify(r0, 0);
+
+    constexpr auto r1 = parse<P>(visitor{}, "a");
+    verify(r1, 1);
+
+    constexpr auto r2 = parse<P>(visitor{}, "a,a");
+    verify(r2, 2);
+
+    constexpr auto r3 = parse<P>(visitor{}, "a,a,a");
+    verify(r3, 3);
+
+    constexpr auto r4 = parse<P>(visitor{}, "a,");
+    verify(r4, unmatched);
+
+    constexpr auto r5 = parse<P>(visitor{}, ",a");
+    verify(r5, unmatched);
+
+    constexpr auto r6 = parse<P>(visitor{}, ",");
+    verify(r6, unmatched);
+}
+
+TEST_CASE("list_production: empty, trailing")
+{
+    using grammar = lex::grammar<test_spec, struct A, struct P>;
+    struct A : lex::token_production<A, grammar, a>
+    {};
+    struct P : lex::list_production<P, grammar, A, comma>
+    {
+        using end_token      = lex::eof_token;
+        using allow_empty    = std::true_type;
+        using allow_trailing = std::true_type;
+    };
+
+    struct visitor
+    {
+        constexpr A operator()(A, a)
+        {
+            return {};
+        }
+
+        constexpr int operator()(P)
+        {
+            return 0;
+        }
+        constexpr int operator()(P, int list, A)
+        {
+            return list + 1;
+        }
+
+        constexpr void operator()(lex::unexpected_token<grammar, A, a>,
+                                  const lex::tokenizer<test_spec>&)
+        {}
+    };
+
+    constexpr auto r0 = parse<P>(visitor{}, "");
+    verify(r0, 0);
+
+    constexpr auto r1 = parse<P>(visitor{}, "a");
+    verify(r1, 1);
+
+    constexpr auto r2 = parse<P>(visitor{}, "a,a");
+    verify(r2, 2);
+
+    constexpr auto r3 = parse<P>(visitor{}, "a,a,a");
+    verify(r3, 3);
+
+    constexpr auto r4 = parse<P>(visitor{}, "a,");
+    verify(r4, 1);
+
+    constexpr auto r5 = parse<P>(visitor{}, ",a");
+    verify(r5, unmatched);
+
+    constexpr auto r6 = parse<P>(visitor{}, ",");
+    verify(r6, unmatched);
 }
